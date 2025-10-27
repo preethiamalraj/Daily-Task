8939be2face1a748a402c2afe6521dbc00544831
preethiamalraj <preethiamalraj273@gmail.com>
2025-10-27 22:26:54 +0530

    Add files via upload

---
+++ day3 c4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int n,l,r;
+	scanf("%d",&n);
+	l=n%10;
+	r=n/10;
+	printf("%d\n%d",l,r);
+	return 0;
+	
+}
---
+++ day3 c5.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int n,sq,cube;
+	scanf("%d",&n);
+	sq=n*n;
+	cube=n*n*n;
+	printf("%d %d",sq,cube);
+	return 0;
+}
